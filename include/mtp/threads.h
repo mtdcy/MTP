7353fcf4044b611a3caf636ffc702b9154451b8f
mtdcy.chen <mtdcy.chen@gmail.com>
2017-11-24 10:24:23 +0800

    prepare README and headers

---
+++ include/mtp/threads.h
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2017 @ mtdcy.chen
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+/**
+ * File: 
+ * Desc:
+ *
+ * Changes:
+ * 20171201     mtdcy   initial version
+ *
+ **/
+
+#ifndef __mtp_threads_h__
+#define __mtp_threads_h__
+
+#include <mtp/types.h>
+#include <pthread.h>
+
+namespace mtp {
+    class Mutex {
+        public:
+            Mutex();
+            virtual ~Mutex();
+
+            void lock();
+            void unlock();
+            bool tryLock();
+
+        private:
+            pthread_mutex_t     mMutex;
+            DISALLOW_EVIALS(Mutex);
+    };
+
+    class Condition {
+        public:
+            Condition();
+            virtual ~Condition();
+
+            void wait(Mutex&);
+            void signal();
+            void broadcast();
+
+        private:
+            pthread_cond_t      mCondition;
+            DISALLOW_EVIALS(Condition);
+    };
+
+    class Thread {
+        public:
+            Thread();
+            virtual ~Thread();
+
+        protected:
+            virtual bool threadLoop() = 0;
+
+        private:
+            pthread_t           mThread;
+            DIALLOW_EVIALS(Thread);
+    };
+};
+
+#endif // __mtp_threads_h__
